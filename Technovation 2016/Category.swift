2bb910784c07293ba1583a4d66e069ccc5373c5e
elswat <ezl@ahsokas-air.attlocal.net>
2016-02-28 10:25:24 -0800

    Added Files

---
+++ Technovation 2016/Category.swift
@@ -0,0 +1,24 @@
+//
+//  Category.swift
+//  Technovation 2016
+//
+//  Created by Ahsoka Tano on 2/21/16.
+//  Copyright (c) 2016 Ahsoka Tano. All rights reserved.
+//
+
+import UIKit
+
+class Category: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Technovation 2016Tests/Technovation_2016Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Technovation_2016Tests.swift
+//  Technovation 2016Tests
+//
+//  Created by Ahsoka Tano on 2/21/16.
+//  Copyright (c) 2016 Ahsoka Tano. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Technovation_2016Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
